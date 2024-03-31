d317cdaa63d7be4f4c1d85a40ba17033ce650a45
Buzu Alexandru <mr.buzu00@gmail.com>
2024-03-31 22:44:31 +0300

    Add cache

--- Lab 5/go2web.py
+++ Lab 5/go2web.py
@@ -1,9 +1,12 @@
-import socket, ssl
-from bs4 import BeautifulSoup, NavigableString
+import socket, ssl, json
+from bs4 import BeautifulSoup
 import sys
 from urllib.parse import urlparse
 
 user_agent = '\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'
+
+with open('cache.json', 'r') as f:
+    cache = json.load(f)
 
 
 def send_request(url):
@@ -17,6 +20,11 @@ def send_request(url):
         ...
     else:
         path = "/"
+
+    if host + path in cache:
+        print('Found in cache')
+        return cache[host + path]
+
     port = 80
     ssl_port = 443
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,7 +35,7 @@ def send_request(url):
     else:
         sock.connect((host, port))
 
-    sock.send(f'GET {path} HTTP/1.1\r\nHost:{host}\r\nConnection: close{user_agent}\r\n\r\n'.encode())
+    sock.send(f'GET {path} HTTP/1.1\r\nHost:{host}\r\nConnection: close\r\n\r\n'.encode())
     response = b''
     data = 1
     while data:
@@ -46,13 +54,21 @@ def send_request(url):
         if header.lower().startswith("content-type"):
             ctype = header.split(": ")[1]
             break
-    # print(response.decode())
-    return response
+    try:
+        decoded_response = response.decode('utf-8')
+    except UnicodeDecodeError:
+        decoded_response = response.decode('ISO-8859-1')
+
+    cache[host + path] = decoded_response
+    with open('cache.json', 'w') as f:
+        json.dump(cache, f)
+
+    return decoded_response
 
 
 def get_page(url):
     res = send_request(url)
-    soup = BeautifulSoup(res.decode(), 'html.parser')
+    soup = BeautifulSoup(res, 'html.parser')
     contents = soup.body.get_text(separator='\n\n', strip=True).strip()
     print(contents)
 
@@ -63,10 +79,18 @@ def google_search(search_terms):
         query += term + '+'
     url = f"https://www.google.com/search?q={query[:-1]}"
     res = send_request(url)
-    soup = BeautifulSoup(res.decode(), 'html.parser')
-    print("\n Top 10 search results: \n")
-    for i, result in enumerate(soup.find_all('a')[16:26], start=1):
-        print(f"{i}. {result.text} - {result['href']}")
+    soup = BeautifulSoup(res, 'html.parser')
+    index = 0
+    for h3_tag in soup.find_all('h3'):
+        index += 1
+        title = h3_tag.getText()
+        print(f'{index}.' + title, end=' - ')
+        parent_anchor = h3_tag.find_parent('a')
+        if parent_anchor:
+            addr = parent_anchor.get('href')
+            if addr:
+                addr = addr.split("?q=")[-1].split('&')[0]
+                print(addr)
 
 
 def main():
